19a10aa1d6d0e51418ab9632ab559b9ecfad8815
CBundy94 <cbundy94@yahoo.com>
2024-11-10 18:04:25 -0600

    Added a new HTML for the purpose of testing the functionality of JS file. JS updated to include dropdown menu in popup. CSS updated to include references made in JS and test HTML.

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const popup = document.getElementById("popup");
+const popupTitle = document.getElementById("popup-title");
+const popupInfo = document.getElementById("popup-info");
+const closePopup = document.getElementById("close-popup");
+
+
+document.getElementById("small-card").addEventListener("click", () => {
+    showPopup("Small Mailbox", "Small units available for $20/month.");
+});
+
+document.getElementById("medium-card").addEventListener("click", () => {
+    showPopup("Medium Mailbox", "Medium units available for $30/month.");
+});
+
+document.getElementById("large-card").addEventListener("click", () => {
+    showPopup("Large Mailbox", "Large units available for $35/month.");
+});
+
+function showPopup(title, info) {
+    popupTitle.textContent = title;
+    popupInfo.textContent = info;
+    popup.style.display = "flex";
+}
+
+closePopup.addEventListener("click", () => {
+    popup.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup) {
+        popup.style.display = "none";
+    }
+});
+
+//Dropdown Menu Trigger
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {});
+  });
